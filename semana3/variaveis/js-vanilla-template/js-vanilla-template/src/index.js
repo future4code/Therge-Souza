7732164181da9a3f6ed92cb2714b9458f4918255
Therge Fonsêca <thergefonseca@gmail.com>
2021-04-19 19:08:45 -0300

    mais uma correçao

--- semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/variaveis/js-vanilla-template/js-vanilla-template/src/index.js
@@ -6,7 +6,7 @@ Se tivesse sido declarada corretamente retornaria "10" na primeira e "10, 5" na 
 
 
 2. Imprimirá erro devida a ausência do CONST, LET ou VAR.
-Se tivesse sido declarada corretamente retornaria "10, 20, 10"
+Se tivesse sido declarada corretamente retornaria "10, 10, 10"
 */
 
 
@@ -16,18 +16,18 @@ Se tivesse sido declarada corretamente retornaria "10, 20, 10"
 let nome
 let idade
 
-console.log(typeof nome, typeof idade)
+console.log(typeof nome, typeof idade);
 
 // retornará undefined pois a variável está sem valor, como não tem nada atribuido não tem como definir o tipo
 
-nome = prompt("Qual o seu nome? ")
-idade = prompt("Qual a sua idade? ")
+nome = prompt("Qual o seu nome? ");
+idade = prompt("Qual a sua idade? ");
 
-console.log(typeof nome, typeof idade)
+console.log(typeof nome, typeof idade);
 
 //Como agora a variável tem valor então pode ser definido o tipo
 
-console.log("Olá " + nome + ", você tem " + idade + " anos.")
+console.log("Olá " + nome + ", você tem " + idade + " anos.");
 
 
 
@@ -35,48 +35,48 @@ console.log("Olá " + nome + ", você tem " + idade + " anos.")
 
 //Exercicio 2
 
-const cidade = prompt("Em que cidade você mora?")
-console.log(cidade)
+const cidade = prompt("Em que cidade você mora?");
+console.log(cidade);
 
-const idade = prompt("Quantos anos você tem?")
-console.log(idade)
+const idade = prompt("Quantos anos você tem?");
+console.log(idade);
 
-const serie = prompt("Qual sua série favorita?")
-console.log(serie)
+const serie = prompt("Qual sua série favorita?");
+console.log(serie);
 
-const cor = prompt("Qual a sua cor predileta?")
-console.log(cor)
+const cor = prompt("Qual a sua cor predileta?");
+console.log(cor);
 
-const irmao = prompt("Você possui quantos irmãos?")
-console.log(irmao)
+const irmao = prompt("Você possui quantos irmãos?");
+console.log(irmao);
 
 
 
 
 //Exercicio 3
 //a
-let comidasFavoritas = ["Mexicana", "Feijoada", "Churrasco", "Acarajé", "Rabada"]
-console.log(comidasFavoritas)
+let comidasFavoritas = ["Mexicana", "Feijoada", "Churrasco", "Acarajé", "Rabada"];
+console.log(comidasFavoritas);
 
 //b
-console.log("Essas são minhas comidas preferidas: ")
-console.log(comidasFavoritas[0])
-console.log(comidasFavoritas[1])
-console.log(comidasFavoritas[2])
-console.log(comidasFavoritas[3])
-console.log(comidasFavoritas[4])
+console.log("Essas são minhas comidas preferidas: ");
+console.log(comidasFavoritas[0]);
+console.log(comidasFavoritas[1]);
+console.log(comidasFavoritas[2]);
+console.log(comidasFavoritas[3]);
+console.log(comidasFavoritas[4]);
 
 //c
-comidasFavoritas[1] = prompt("Qual sua comida preferida?")
-console.log(comidasFavoritas)
+comidasFavoritas[1] = prompt("Qual sua comida preferida?");
+console.log(comidasFavoritas);
 
 
 
 //Exercicio 4
 
-const perguntas = ["Você toma café?", "Você já viajou para fora do Brasil?", "Você pratica esporte?"]
-const respostas = [false, true, true]
+const perguntas = ["Você toma café?", "Você já viajou para fora do Brasil?", "Você pratica esporte?"];
+const respostas = [false, true, true];
 
-console.log(perguntas[0, respostas[0]])
-console.log(perguntas[1], respostas[1])
-console.log(perguntas[2], respostas[2])
+console.log(perguntas[0], respostas[0]);
+console.log(perguntas[1], respostas[1]);
+console.log(perguntas[2], respostas[2]);
