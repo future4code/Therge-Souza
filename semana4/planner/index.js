4500130473b729034ed3c21ba4faf3d75ff99178
Therge Fonsêca <thergefonseca@gmail.com>
2021-04-28 10:49:37 -0300

    semana4-aula15 atualizado

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,19 +1,29 @@
-// criar a função ao botão de criar tarefas
+//Contador para adicionar DIV's
 let contador = 1
-
+    // criar a função ao botão de criar tarefas
 function criarTarefa() {
 
     //Seleciona os inputs pela ID
     const adicionarTarefa = document.getElementById('tarefa').value
     const adicionarNoDia = document.getElementById('dias-semana').value
 
-
-
+    //Adiciona os valores dos inputs no campo selecionado, utilizando o value
     if (adicionarTarefa !== "") {
-        //Adiciona os valores dos inputs no campo selecionado, utilizando o value
         const adicionarPlanner = document.getElementById(`${adicionarNoDia}`)
-        adicionarPlanner.innerHTML += `<p id=${contador}}> ${contador} - ${adicionarTarefa}</p>`
+        adicionarPlanner.innerHTML += `<p class="riscado" id=${contador}}> ${contador} - ${adicionarTarefa}</p>`
         contador += 1
+    } else {
+        alert("O campo de tarefas está em branco")
     }
+}
+
+const riscarCampos = document.getElementsByTagName(this)
+
+function textoRiscado(riscarCampos) {
+
+    riscarCampos.addEventListener('click', function riscando() {
+        riscarCampos.classList.add('riscado')
+    })
+
 
 }
