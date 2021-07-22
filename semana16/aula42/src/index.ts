686f28a9a92e14f59b9379809f2a40921c0589dc
Therge Fonseca <thergefonseca@gmail.com>
2021-07-22 07:30:03 -0300

    treinando aula42

--- semana16/aula42/src/index.ts
+++ semana16/aula42/src/index.ts
@@ -41,3 +41,94 @@ type Colors = {
 const coresFavoritas: Colors = {
     class: RainbowColor.AZUL
 }
+
+//Exercicio 2
+// A
+function obterEstatisticas(numeros:number[]){
+
+    const numerosOrdenados = numeros.sort(
+        (a:number, b:number) => a - b
+    )
+
+    let soma:number = 0
+
+    for (let num of numeros) {
+        soma += num
+    }
+
+    type tipoNumeros = {
+        maior: number,
+        menor: number,
+        media: number
+    }
+
+    const estatisticas:tipoNumeros = {
+        maior: numerosOrdenados[numeros.length - 1],
+        menor: numerosOrdenados[0],
+        media: soma / numeros.length
+    }
+
+    return estatisticas
+}
+
+//B todas são tipo Number
+
+
+//C 
+type idadesType = {
+    numeros: number,
+    obterEstatisticas: any
+}
+
+const amostraDeIdades = {
+
+    numeros: [21, 18, 65, 44, 15, 18],
+
+    obterEstatisticas: () => {}
+}
+
+//Exercicio 3
+//A
+
+type tipagemPost = {
+    autor: string,
+    texto: string
+}
+const posts:tipagemPost[] = [
+  {
+    autor: "Alvo Dumbledore",
+    texto: "Não vale a pena viver sonhando e se esquecer de viver"
+  },
+  {
+    autor: "Severo Snape",
+    texto: "Menos 10 pontos para Grifinória!"
+  },
+  {
+    autor: "Hermione Granger",
+    texto: "É levi-ô-sa, não levio-sá!"
+  },
+  {
+    autor: "Dobby",
+    texto: "Dobby é um elfo livre!"
+  },
+  {
+    autor: "Lord Voldemort",
+    texto: "Avada Kedavra!"
+  }
+]
+
+
+//B
+//As entradas são os posts e as informações do autor e as saida é a filtragem de post por autor
+
+
+function buscarPostsPorAutor(
+    posts:tipagemPost[], 
+    autorInformado:string
+): tipagemPost[] | [] {
+  return posts.filter(
+    (post) => {
+      return post.autor === autorInformado
+    }
+  )
+}
